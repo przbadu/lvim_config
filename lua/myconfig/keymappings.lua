f5cd17c4871cb80e96780b352c5fc1b2334944a9
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2023-09-26 09:31:49 +0545

    Save all and quit C-s C-s

--- lua/myconfig/keymappings.lua
+++ lua/myconfig/keymappings.lua
@@ -15,3 +15,7 @@ vim.keymap.set('n', '<F10>', require 'dap'.step_over)
 vim.keymap.set('n', '<F11>', require 'dap'.step_into)
 vim.keymap.set('n', '<F12>', require 'dap'.step_out)
 -- vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
+
+-- Save and close all the windows (:wqa)
+vim.keymap.set({ "i", "n" }, "<C-s><C-s>", "<cmd>silent! xa<cr><cmd>qa<cr>", { desc = "Save all and quit" })
+
